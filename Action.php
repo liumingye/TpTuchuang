e7287aec9b117394d47f228edfa70a482a3e3add
刘明野 <898310895@qq.com>
2021-02-21 13:50:22 +0800

    Update Action.php

--- Action.php
+++ Action.php
@@ -3,78 +3,91 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
     exit;
 }
 
-?>
-
-<?php
-
-include_once 'Option.php';
-
 class TpTuchuang_Action extends Widget_Abstract_Contents implements Widget_Interface_Do
 {
+    /**
+     * 判断用户是否登录
+     * @return bool
+     * @throws Typecho_Widget_Exception
+     */
+    public static function check_login()
+    {
+        //http与https相互独立
+        return (Typecho_Widget::widget('Widget_User')->hasLogin());
+    }
+
+    private static function getSafeName(&$name)
+    {
+        $name = str_replace(array('"', '<', '>'), '', $name);
+        $name = str_replace('\\', '/', $name);
+        $name = false === strpos($name, '/') ? ('a' . $name) : str_replace('/', '/a', $name);
+        $info = pathinfo($name);
+        $name = substr($info['basename'], 1);
+
+        return isset($info['extension']) ? strtolower($info['extension']) : '';
+    }
 
     public function action()
     {
+        if (!$this->check_login()) return false;
         switch ($_GET['action']) {
             case 'upload':
-                $option = new Option;
-                if (isset($_GET['key']) && $_GET['key'] == md5($option->key)) {
-                    $this->upload($_FILES['file']);
-                }
+                $this->upload($_FILES['file']);
                 break;
-
         }
+    }
+
+    public function checkFileType($ext)
+    {
+        return in_array($ext, ['jpg', 'gif', 'jpeg', 'png']);
     }
 
     public function upload($file)
     {
-        if (is_uploaded_file($file['tmp_name'])) {
-            $arr = pathinfo($file['name']);
-            $ext_suffix = $arr['extension'];
-            $allow_suffix = array('jpg', 'gif', 'jpeg', 'png');
-            if (!in_array($ext_suffix, $allow_suffix)) {
-                $this->msg(['code' => 1, 'msg' => '上传格式不支持']);
-            }
-            $new_filename = time() . rand(100, 1000) . '.' . $ext_suffix;
-            if (move_uploaded_file($file['tmp_name'], $new_filename)) {
-                $data = $this->upload2('https://kfupload.alibaba.com/mupload', $new_filename);
-                $pattern = '/"url":"(.*?)"/';
-                preg_match($pattern, $data, $match);
-                @unlink($new_filename);
-                if ($match && $match[1] != '') {
-                    $this->msg(['code' => 0, 'msg' => $match[1]]);
+        $this->msg(['code' => 1, 'msg' => '移动文件失败']);
+        try {
+            if (is_uploaded_file($file['tmp_name'])) {
+                $ext = $this->getSafeName($file['name']);
+                if (!$this->checkFileType($ext)) {
+                    $this->msg(['code' => 1, 'msg' => '上传格式不支持']);
+                }
+                $fileName = sprintf('%u', crc32(uniqid())) . '.' . $ext;
+                if (move_uploaded_file($file['tmp_name'], $fileName)) {
+                    $data = $this->uploadAlibaba($fileName);
+                    @unlink($fileName);
+                    $data = json_decode($data);
+                    if (isset($data->url)) {
+                        $this->msg(['code' => 0, 'msg' => $data->url]);
+                    } else {
+                        $this->msg(['code' => 1, 'msg' => '上传失败']);
+                    }
                 } else {
-                    $this->msg(['code' => 1, 'msg' => '上传失败']);
+                    $this->msg(['code' => 1, 'msg' => '移动文件失败']);
                 }
             } else {
                 $this->msg(['code' => 1, 'msg' => '上传数据有误']);
             }
-        } else {
-            $this->msg(['code' => 1, 'msg' => '上传数据有误']);
+        } catch (\Throwable $th) {
+            $this->msg(['code' => 1, 'msg' =>  $th->getMessage()]);
         }
     }
 
-    public function upload2($url, $file)
+    public function uploadAlibaba($file)
     {
-        return $this->get_url($url, [
+        $post = [
             'scene' => 'aeMessageCenterImageRule',
             'name' => $file,
             'file' => new \CURLFile(realpath($file)),
-        ]);
-    }
-
-    public function get_url($url, $post)
-    {
+        ];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_URL, 'https://kfupload.alibaba.com/mupload');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_USERAGENT, 'iAliexpress/6.22.1 (iPhone; iOS 12.1.2; Scale/2.00)');
-        if ($post) {
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        }
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
         if (curl_exec($ch) === false) {
             echo 'Curl error: ' . curl_error($ch);
         }
